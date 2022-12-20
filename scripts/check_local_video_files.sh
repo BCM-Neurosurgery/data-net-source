c18bcbb776b785df115a56aa73d07d8ee47ed881
gsquist <gsquist@cs.washington.edu>
2022-12-20 12:58:03 -0800

    starting code for moving video file naming check from python to bash

--- scripts/check_local_video_files.sh
+++ scripts/check_local_video_files.sh
@@ -10,5 +10,22 @@
 date=$(date --date="today" +"%Y%m%d")
 video_path="/media/DATA/rcs07/raw_videos/"$date"/"
 
+for dir in "$video_path"*/;do
+  echo $dir
+  for file in $dir*.avi ;do
+    readarray -d / -t strarr <<<"$file" #split a string based on the delimiter '/'
+    readarray -d _ -t strarr <<<"${strarr[8]}"
+    vid_name="${strarr[1]}"
+    readarray -d - -t strarr <<<"${vid_name}" #split a string based on the delimiter '-'
+    file_hour="${strarr[3]}"
+    if ! (($file_hour % 2)); then
+      echo "$file_hour divisible by 2."
+    fi
+  done
+done
+#read n
+#if ! ((n % 2)); then
+#    echo "$n divisible by 2."
+#fi
 
-python ../video_handler.py "$video_path"
+#python ../video_handler.py "$video_path"
