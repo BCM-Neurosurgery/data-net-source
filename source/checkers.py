0932d2ce93466c3ecd0f5d6b991033250ad4a81d
TomekFraczek <tfraczek@uw.edu>
2024-01-08 15:13:55 -0800

    Added basically ready FileCheckerMixin

--- source/checkers.py
+++ source/checkers.py
@@ -1,5 +1,6 @@
 import json
 import os
+from datetime import datetime
 from abc import ABC, abstractmethod
 
 
@@ -12,6 +13,7 @@ def load_success_log(log_location):
 
 class BaseChecker(ABC):
     """Base class for all checkers that defines the interface"""
+    log_filename = 'upload_log.json'
 
     @property
     @abstractmethod
@@ -24,12 +26,24 @@ class BaseChecker(ABC):
         """"""
         return {}  # Return a dict describing the data that needs to be uploaded
 
+    @abstractmethod
+    def save(self, completed):
+        """"""
+
+    def describe_parser(self):
+        return {
+            "git commit": "commitHash",  # TODO: implement commit hashing
+            "base": str(type(self)),
+            "checker": self.checker_name,
+            "transformer": self.transformer_name,  # Defined in the TransformerMixin
+            "uploader": self.uploader_name  # Defined in the UploaderMixin
+        }
+
 
 class DirectoryCheckerMixin(BaseChecker):
     """
     Mixin to a parser that checks whether an entire directory has already been uploaded or not
     """
-
     checker_name = "DirectoryChecker"
 
     def check(self):
@@ -44,7 +58,7 @@ class DirectoryCheckerMixin(BaseChecker):
         ]
 
         # TODO: the log will need to be parsed somehow, not sure what other info we will save here
-        successes = load_success_log(os.path.join(source_dir, 'upload_log.json'))
+        successes = load_success_log(os.path.join(source_dir, self.log_filename))
         already_uploaded = [os.path.join(obj['checked'], obj['uploaded']) for obj in successes]
 
         to_upload = [
@@ -56,6 +70,52 @@ class DirectoryCheckerMixin(BaseChecker):
     def save(self, completed):
         # TODO: Implement a save
         pass
+
+
+class FileCheckerMixin(BaseChecker):
+
+    checker_name = "FileChecker"
+
+    def check(self):
+        """Check only the individual files in a directory if they have been uploaded or not"""
+        source_dir = self.source_location  # Defined in ParserBase
+        files_here = [
+            filename for filename in os.listdir(source_dir)
+            if os.path.isfile(os.path.join(source_dir, filename) and filename != self.log_filename)
+        ]
+
+        successes = load_success_log(os.path.join(source_dir, self.log_filename))
+        uploaded_files = [success['uploaded'] for success in successes]
+
+        to_upload = [
+            filename for filename in files_here
+            if filename not in uploaded_files
+        ]
+        return to_upload
+
+    def build_log_entry(self, entry_data):
+        return {
+            'type': 'file',
+            'checked': self.source_location,
+            'uploaded': entry_data['filename'],
+            'status': entry_data['status'],
+            'parser': self.describe_parser(),
+            'timestamp': datetime.now().timestamp()
+        }
+
+    def save(self, completed):
+        """Log the files the that have been uploaded, along with all errors"""
+        with open(os.path.join(self.source_location, self.log_filename)) as log:
+            logged_data = json.load(log)
+
+        new_success = [self.build_log_entry(success) for success in completed['success']]
+        logged_data['success'].extend(new_success)
+
+        new_failure = [self.build_log_entry(failure) for failure in completed['failure']]
+        logged_data['failure'].extend(new_failure)
+
+        with open(os.path.join(self.source_location, self.log_filename), 'w') as log:
+            json.dump(logged_data, log, indent=2)
 
 
 class RuneAPICheckerMixin(BaseChecker):
