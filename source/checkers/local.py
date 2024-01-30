090adec11e5e5fa50789cfb5a9fadaf968aa3f92
TomekFraczek <tfraczek@uw.edu>
2024-01-30 15:04:10 -0800

    Make the file cheker recursive

--- source/checkers/local.py
+++ source/checkers/local.py
@@ -38,24 +38,41 @@ class DirectoryCheckerMixin(BaseChecker):
 
 
 class FileCheckerMixin(BaseChecker):
+    """
+    Mixin to add a checker that recursively searches for new files
+
+
+    """
 
     checker_name = "FileChecker"
 
-    def check(self):
+    def check(self, source_dir=None):
         """Check only the individual files in a directory if they have been uploaded or not"""
-        source_dir = self.source_location  # Defined in ParserBase
-        files_here = [
-            filename for filename in os.listdir(source_dir)
-            if os.path.isfile(os.path.join(source_dir, filename) and filename != self.log_filename)
-        ]
 
-        successes = load_success_log(os.path.join(source_dir, self.log_filename))
+        # Draw the source location from the class settings if not passed explicitly under recursion
+        source_dir = self.source_location if source_dir is None else source_dir
+        log_dir = self.source_location  # Log is always saved at the top level
+
+        successes = load_success_log(os.path.join(log_dir, self.log_filename))
         uploaded_files = [success['uploaded'] for success in successes]
 
-        to_upload = [
-            filename for filename in files_here
-            if filename not in uploaded_files
-        ]
+        # Determine which of the files here need to be uploaded
+        to_upload = []
+        for item_here in os.listdir(source_dir):
+            full_path = os.path.join(source_dir, item_here)
+
+            # Skip any files and folders that have already been logged as complete
+            if full_path in uploaded_files:
+                continue
+
+            # For any files besides the logfile check if they've been uploaded
+            if os.path.isfile(full_path) and item_here != self.log_filename:
+                to_upload.append(full_path)
+
+            # For any directories that have not been marked as completed, process recursively
+            elif os.path.isdir(full_path):
+                to_upload.extend(self.check(full_path))
+
         return to_upload
 
     def build_log_entry(self, entry_data):
@@ -80,3 +97,16 @@ class FileCheckerMixin(BaseChecker):
 
         with open(os.path.join(self.source_location, self.log_filename), 'w') as log:
             json.dump(logged_data, log, indent=2)
+
+
+class StreamedFileChecker(BaseChecker):
+    """Checker to load files from a directory that is being actively streamed to"""
+
+    def __init__(self):
+        initialize_files = ['*.ccf', '*.csr', '*.sif', '*.toc']
+        streamed_files = ['*.nev', '*.ns3', '*.ns5']
+        file_duration = None
+
+
+
+
