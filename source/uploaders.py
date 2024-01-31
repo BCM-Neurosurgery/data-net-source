79f8ed8441b1d2222686cb8de685cc5b58608e78
TomekFraczek <tfraczek@uw.edu>
2024-01-30 16:15:34 -0800

    Added a simple copy transformer, mainly for testing purposes

--- source/uploaders.py
+++ source/uploaders.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+import sys
 from abc import abstractmethod, ABC
 
 
@@ -10,7 +13,7 @@ class BaseUploader(ABC):
         return "BaseUploader"
 
     @abstractmethod
-    def upload(self, ):
+    def upload(self, ready):
         return {}  # Should return a dict describing successful uploads and failures
 
 
@@ -21,6 +24,43 @@ class BucketUploaderMixin(BaseUploader):
 
     uploader_name = "BucketUploaderMixin"
 
-    def upload(self, ):
+    def upload(self, ready):
         raise NotImplementedError
+
+
+class CopyUploaderMixin(BaseUploader):
+
+    uploader_name = "SimpleCopyMixin"
+
+    def upload(self, ready):
+
+        errors = []
+        successes = []
+
+        for filename in ready:
+            destination = 'Failed to determine!'
+            try:
+                rel_filepath = os.path.relpath(filename, start=self.source_location)
+                destination = os.path.join(self.target_location, rel_filepath)
+                shutil.copy(filename, destination)
+            except Exception as e:
+                errors.append({
+                    'type': 'upload failure',
+                    'location': 'CopyUploaderMixin.upload',
+                    'filename': filename,
+                    'destination': destination,
+                    'error': str(e),
+                    'trace': sys.exc_info()
+                })
+            else:
+                successes.append({
+                    'type': 'upload success',
+                    'filename': filename,
+                    'destination': destination,
+                })
+
+        return {
+            'successes': successes, 'errors': errors
+        }
+
 
