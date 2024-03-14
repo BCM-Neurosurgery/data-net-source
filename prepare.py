7e55344178f5936d4bcb955c58c0d19cf597f4b3
TomekFraczek <tfraczek@uw.edu>
2024-03-14 09:54:02 -0700

    Added a simple script to prepare a source parser

---
+++ prepare.py
@@ -0,0 +1,34 @@
+"""
+Run any preparation steps required before a source parser can be run
+"""
+
+import os
+import json
+import argparse
+from run import load_config, load_parser
+
+
+EMPTY_LOG = {
+    "success": [],
+    "failure": []
+}
+
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('config_file', type=str,
+                            help='Path to the config file that specifies the parser to run')
+    args = arg_parser.parse_args()
+
+    config = load_config(args.config_file)
+    # parser = load_parser(config)
+    # parser.prepare()
+
+    # Temp first step, make a simple empty upload log
+    log_dir = config['parser']['init']['middle']
+    os.makedirs(log_dir, exist_ok=True)
+    with open(os.path.join(log_dir, 'upload_log.json'), 'w') as logfile:
+        json.dump(EMPTY_LOG, logfile)
+
+
+
