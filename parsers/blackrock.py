d0033f698a27878fc9984ca5f0594941fda81501
TomekFraczek <tfraczek@uw.edu>
2024-03-13 22:48:46 -0700

    Update brk checker settings

--- parsers/blackrock.py
+++ parsers/blackrock.py
@@ -5,9 +5,9 @@ from source.uploaders.simple import CopyUploaderMixin
 
 class BlackrockChecker(StreamedFileCheckerMixin):
     initialize_files = ['.ccf', '.csr', '.sif', '.toc']
-    streamed_files = ['.nev', '.ns3', '.ns5']
-    stream_rate = 240.0
-    reliability_factor = 2.0
+    streamed_files = ['.nev'] + [f'.ns{i}' for i in range(1, 10)]
+    stream_rate = 60*10  # New file every 10 minutes
+    reliability_factor = 1.1  # File durations are quite reliable, approx 1 min buffer in case of rounding error
 
 
 class BlackrockParser(BlackrockChecker, CopyUploaderMixin, ParserCommon):
