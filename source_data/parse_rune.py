842f8d52c3304bd3ad56c80c9511a5953b084ccc
TomekFraczek <tfraczek@uw.edu>
2024-03-13 22:45:46 -0700

    Finish reorganizing files

--- source_data/parse_rune.py
+++
@@ -1,9 +0,0 @@
-from parsers.WatchParser import WatchParser
-
-test = WatchParser(
-    source=None,
-    middle=r'D:\Work\DataNet\TestData\parsers\rune_parser',
-    target=r'D:\Work\DataNet\TestData\lake\rune'
-)
-
-test.check()
