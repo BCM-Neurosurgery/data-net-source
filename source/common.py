4e3318da54f7c3870966dc3a0a87fad4663f3f94
TomekFraczek <tfraczek@uw.edu>
2024-03-25 15:21:17 -0700

    Added a file and sentry logger

--- source/common.py
+++ source/common.py
@@ -1,3 +1,4 @@
+import logging
 from abc import ABC, abstractmethod
 
 
@@ -29,6 +30,7 @@ class ParserCommon(ABC):
         self.source_location = source
         self.middle_location = middle
         self.target_location = target
+        self.loggers = []
 
     def process(self):
         to_do = self.check()
@@ -83,9 +85,47 @@ class ParserCommon(ABC):
         """
         Get the metadata about the data being parsed as a dictionary
 
-        Uploaders should override this method to add additional metadata, and include the contents here
+        Uploader should override this method to add additional metadata, and include the contents here
         :return:
         """
         meta = {}
         # TODO: Add some basic common metadata
         return meta
+
+    def log(self, message, level=logging.INFO):
+        """Generic method to forward logging to all loggers saved for parser"""
+        for logger in self.loggers:
+            logger.log(message, level=level)
+
+    def make_loggers(self, log_config: dict):
+        """Prepare the python loggers to manage user notifications and log messages"""
+        logging.basicConfig(level=logging.DEBUG)
+
+        if 'file' in log_config:
+            from logging.handlers import RotatingFileHandler
+            file_logger = logging.getLogger('file')
+            file_logger.setLevel(log_config['file']['level'])
+            handler = RotatingFileHandler(
+                log_config['file']['filepath'],
+                maxBytes=log_config['file']['max_size'],
+                backupCount=log_config['file']['max_files']
+            )
+            file_logger.addHandler(handler)
+            self.loggers.append(file_logger)
+
+        if 'sentry' in log_config:
+            import sentry_sdk as sentry
+            from sentry_sdk.integrations.logging import LoggingIntegration
+
+            sentry.init(
+                dsn=log_config['sentry']['dsn'],
+                integrations=[
+                    sentry.integrations.logging.LoggingIntegration(
+                        level=log_config['sentry']['level'],  # Capture info and above as breadcrumbs
+                        event_level=log_config['sentry']['event_level']  # Send records as events
+                    ),
+                ],
+            )
+            self.loggers.append(logging.getLogger('sentry_sdk.errors'))
+            self.loggers.append(logging.getLogger('sentry_sdk'))
+
