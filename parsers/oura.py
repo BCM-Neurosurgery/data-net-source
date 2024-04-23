cdf01038b2aa0184969f18fa8890d08acc62a9e6
TomekFraczek <tfraczek@uw.edu>
2024-04-23 16:07:04 -0700

    Actuallly remove Transformer

--- parsers/oura.py
+++ parsers/oura.py
@@ -1,8 +1,7 @@
 from source.common import ParserCommon
 from source.checkers.api import OuraAPIDocumentChecker
 from source.uploaders.simple import CopyUploaderMixin
-from source.transformers.api import OuraDocTransformer
 
 
-class OuraRingDocumentParser(OuraAPIDocumentChecker, OuraDocTransformer, CopyUploaderMixin, ParserCommon):
+class OuraRingDocumentParser(OuraAPIDocumentChecker, CopyUploaderMixin, ParserCommon):
     """"""
