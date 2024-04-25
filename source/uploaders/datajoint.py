6000540b3cfbccaaa215ba2de48bcc5e2217fa93
TomekFraczek <tfraczek@uw.edu>
2024-04-25 14:49:28 -0700

    Don't instantiate the schema until the upload function

--- source/uploaders/datajoint.py
+++ source/uploaders/datajoint.py
@@ -18,12 +18,8 @@ class DataJointUploader(BaseUploader, ABC):
 
 class EMUBlackrockDJUploader(DataJointUploader):
 
-    from emu24 import EMU24 as emu_schema
     import datajoint as dj
-
     uploader_name = 'EMUNSPDataJointUploader'
-    schema = emu_schema
-    datajoint = None
 
     def connect_to_database(self):
         """Connect to the SQL database using the info in the configuration"""
@@ -44,6 +40,8 @@ class EMUBlackrockDJUploader(DataJointUploader):
         self.dj.config['database.password'] = sql_config['password']
         self.dj.config['database.port'] = sql_config['port']
 
+        self.dj.config['stores'] = self.target_location['stores']
+
         print('\n')
         print('POST CONFIG:')
         print('Datajoint')
@@ -57,6 +55,10 @@ class EMUBlackrockDJUploader(DataJointUploader):
         self.dj.conn()
 
     def upload(self, ready):
+
+        # Define the schema to use by importing the appropriate script
+        from emu24 import EMU24 as schema
+
         for filename in ready['to upload']:
 
             # We know what the path will be of the form ...preamble/emu/patientDatafile/modality/otherbits...
@@ -68,7 +70,7 @@ class EMUBlackrockDJUploader(DataJointUploader):
             admission_id = admission.split('Admission')[-1]
 
             filetype = filename.split('.')[-1]
-            file_table = getattr(self.schema, f'{filetype.capitalize()}Chunks')
+            file_table = getattr(schema, f'{filetype.capitalize()}Chunks')
             file_table.insert({
                 'patient': patient_id,
                 'admission': admission_id,
