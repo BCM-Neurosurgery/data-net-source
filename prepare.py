f982a99cdbdf957b31108603b8f21b63c6ec470a
TomekFraczek <tfraczek@uw.edu>
2024-05-13 11:26:33 -0700

    Update parser to use dict version of init config

--- prepare.py
+++ prepare.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     # parser.prepare()
 
     # Temp first step, make a simple empty upload log
-    log_dir = config['parser']['init']['middle']
+    log_dir = config['parser']['init']['middle']['path']
     os.makedirs(log_dir, exist_ok=True)
     with open(os.path.join(log_dir, 'upload_log.json'), 'w') as logfile:
         json.dump(EMPTY_LOG, logfile)
