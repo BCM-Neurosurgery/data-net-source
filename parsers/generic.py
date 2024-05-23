df8f6a2794ea5005684ec0fbca2673e3dcdaed3f
TomekFraczek <TomekFraczek@users.noreply.github.com>
2024-05-23 14:19:44 -0500

    SCP Upgrade and Dynamic Parser Building
    * Move SCP uploader to the ssh file
    
    * Return both ssh and scp clients
    
    * Explicitly cast filenames to str in error dict
    
    * Fix traceback formatting
    
    * SCPUploader must inherit from BaseUploader
    
    * Added a generic parser location
    
    * Initial version of the dynamically build source parsers
    
    * Minor dynamic class creation fixes, seems to be working fine

---
+++ parsers/generic.py
@@ -0,0 +1,8 @@
+from source.checkers.local import FileCheckerMixin
+from source.uploaders.ssh import SCPUploaderMixin
+from source.common import ParserCommon
+
+
+class SCPFileParser(FileCheckerMixin, SCPUploaderMixin, ParserCommon):
+    """Basic source parser for moving all files in a directory to a remote endpoint via SCP"""
+
---
+++ source/uploaders/ssh.py
@@ -0,0 +1,91 @@
+import os
+import sys
+import json
+import pathlib
+import traceback
+import numpy as np
+import pandas as pd
+
+from paramiko import SSHClient
+from scp import SCPClient
+
+from source.uploaders.base import BaseUploader
+
+
+class SCPUploaderMixin(BaseUploader):
+    """
+    This uploader expects a target location of the form of a dict as below
+    {
+      "ssh-config": {dict of kwargs passed to paramiko.SSHClient},
+      "path": /base/path/on/remote"
+    }
+    """
+
+    uploader_name = 'SCPUploader'
+
+    def get_ssh_transport(self):
+        # Set up the ssh client and associated scp transport
+        self.info('Connecting to remote host...')
+        ssh = SSHClient()
+        ssh.load_system_host_keys()
+        ssh.connect(**self.target_location['ssh-config'])
+        scp = SCPClient(ssh.get_transport())
+        self.info('Established connection')
+        return ssh, scp
+
+    def upload(self, ready):
+
+        errors = ready['failure']
+        successes = []
+        all_rates = []
+
+        remote_target = self.target_location['path']
+        ssh, scp = self.get_ssh_transport()
+
+        for filename in ready['to upload']:
+            destination = 'Failed to determine!'
+            try:
+                self.info(f'Uploading {filename}')
+                rel_filepath = os.path.relpath(filename, start=self.source_location)
+
+                # Make sure the destination folder exists using ssh. We assume a *nix destination
+                # This solution is a bit hacky, likely executes a lot more commands than necessary
+                folder_path = pathlib.Path(remote_target, os.path.dirname(rel_filepath))
+                outputs = ssh.exec_command(f'mkdir -p {folder_path.as_posix()}')
+
+                # Actually do the file copy
+                destination = pathlib.Path(remote_target, rel_filepath)
+                self.info(f'  Moving to {destination}')
+                size = os.path.getsize(filename) / 1024 ** 2  # File size in MB
+                rate = self.time_upload(size, scp.put, filename, destination.as_posix())
+                self.info(f'  Upload complete. ({np.round(size, 2)} MB at {np.round(rate, 2)} MB/s)')
+                if size > 1.0:
+                    all_rates.append(rate)
+            except Exception as e:
+                error_dict = {
+                    'type': 'upload failure',
+                    'location': 'SCPUploaderMixin.upload',
+                    'filename': str(filename),
+                    'destination': str(destination),
+                    'error': str(e),
+                    'trace': traceback.format_exception(*sys.exc_info())
+                }
+                errors.append(error_dict)
+                self.warning('An upload failed!')
+                self.warning(json.dumps(error_dict, indent=2))
+            else:
+                successes.append({
+                    'type': 'upload success',
+                    'filename': filename,
+                    'destination': destination,
+                    'transfer rate': rate
+                })
+
+        # Make sure we close the transports
+        scp.close()
+        ssh.close()
+        self.info(f'Average transfer rate {round(np.nanmean(all_rates), 2)} MB/s')
+
+        return {
+            'success': successes, 'failure': errors
+        }
