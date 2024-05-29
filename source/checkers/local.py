41e9856ad95db4c8a376ad9fdf1e70f3133d48e5
TomekFraczek <tfraczek@uw.edu>
2024-05-29 14:27:50 -0700

    Added a IndicatorFileChecker

--- source/checkers/local.py
+++ source/checkers/local.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 import shutil
 from datetime import datetime
 
@@ -227,3 +228,78 @@ class StreamedFileCheckerMixin(FileCheckerMixin):
                 to_upload.append(filepath)
 
         return {'to do': to_upload, 'failure': failure}
+
+
+class IndicatorFileCheckerMixin(FileCheckerMixin):
+    """
+    Checker that uses a set of indicator files to limit the directories to search for new files
+
+    This can be very useful if you have many small patient directories that should not be deleted, but the majority
+    of them will not be updated. By default, all files in the directory which contain only letters or numbers (ie a
+    patient ID) as well as an optional file extension will be matched. Everything before the file extension will be
+    considered the name of a directory inside the "path" directory to check for new files. For example:
+    If the indicator directory contains the files:
+        - P01.txt
+        - P02
+        - P32_ignore.txt
+    Then the Checker will look for new files in:
+        - <path>/PO1/
+        - <path>/PO2/
+    All other directories and loose files in the <path> directory will be ignored
+
+    Requires a source config of the following format
+    {
+        "path": "/path/to/directory/with/data",
+        "indicator_dir": "/path/to/directory/with/indicator_files"
+    }
+    The fields in this source:
+      - "indicator-dir": absolute path of the directory where the indicator files are found
+
+    Additional settings
+      - indicator_regex: regular expression that defines how the indicator files should be interpreted, must contain
+        exactly at least one capturing group, and match the part string only once. This regex will be applied to all the
+        files in the indicator_dir directory.
+      - indicator_format: The results (capturing groups) of the regex match will be formatted using this format string.
+        The result will then be appended to the "path" setting to define the directories that will be searched for new
+        files.
+
+    """
+
+    source = {
+        "path": "",
+        "indicator_dir": ""
+    }
+
+    indicator_regex = "([a-zA-Z0-9]+)[.a-zA-Z0-9]*"
+    indicator_format = "{}"
+
+    def parse_indicators(self):
+
+        indicators = os.listdir(self.source['indicator_dir'])
+        check_locations = []
+
+        for indicator in indicators:
+            result = re.search(self.indicator_regex, indicator)
+            if result is None:
+                continue  # This file does not match the indicator regex
+            else:
+                formatted = self.indicator_format.format(result.groups)
+                check_locations.append(os.path.join(self.source['path'], formatted))
+
+        return check_locations
+
+    def check(self, source_dir=None):
+        """Recursively check the contents of a subset of the directories in the given path"""
+
+        to_check = self.parse_indicators()
+        to_do = []
+        failure = []
+
+        for directory in to_check:
+            found_here = super().check(source_dir=directory)
+            to_do.extend(found_here['to do'])
+            failure.extend(found_here['failure'])
+
+        return {'to do': to_do, 'failure': failure}
+
+
