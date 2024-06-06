e1c64fd93907b9c2edf372b0932ee5f9962fb7a9
TomekFraczek <tfraczek@uw.edu>
2024-06-06 11:47:26 -0700

    Add a SCP-enabled video parser

--- parsers/video.py
+++ parsers/video.py
@@ -5,11 +5,15 @@ from source.uploaders.simple import CopyUploaderMixin, SCPUploaderMixin
 
 class StreamedVideoCheckerMixin(StreamedFileCheckerMixin):
     """Streamed file checker specifically for video files"""
-    initialize_files = ['.json']
-    streamed_files = ['.mp4']
+    initialize_files = []
+    streamed_files = ['.mp4', '.json']
     stream_rate = 60*10  # New file every 10 minutes
     reliability_factor = 1.1  # File durations are quite reliable, approx 1 min buffer in case of rounding error
 
 
 class VideoParser(StreamedVideoCheckerMixin, CopyUploaderMixin, ParserCommon):
+    """"""
+
+
+class VideoSCPParser(StreamedVideoCheckerMixin, SCPUploaderMixin, ParserCommon):
     """"""
