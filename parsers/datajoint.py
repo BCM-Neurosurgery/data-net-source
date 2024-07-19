163ac080289161a55f3a570215a1368a80632fd4
TomekFraczek <tfraczek@uw.edu>
2024-07-19 15:19:16 -0700

    Add in a custom checker for BRKDatajoint that combines indicator and streamed checkers

--- parsers/datajoint.py
+++ parsers/datajoint.py
@@ -1,7 +1,28 @@
 from source.common import ParserCommon
 from parsers.blackrock import BlackrockChecker
+from source.checkers.local import IndicatorFileCheckerMixin
 from source.uploaders.datajoint import EMUBlackrockDJUploader
 
 
-class DataLakeBRKParser(BlackrockChecker, EMUBlackrockDJUploader, ParserCommon):
+class DatalakeBRKChecker(BlackrockChecker, IndicatorFileCheckerMixin):
+    """
+    """
+
+    def check(self):
+        to_check = self.parse_indicators()
+        to_do = []
+        failure = []
+
+        for directory in to_check:
+            try:
+                found_here = BlackrockChecker.check(self)
+            except FileNotFoundError as e:
+                raise FileNotFoundError(f'Indicated patient not found! \n  {e.filename}')
+            to_do.extend(found_here['to do'])
+            failure.extend(found_here['failure'])
+
+        return {'to do': to_do, 'failure': failure}
+
+
+class DataLakeBRKParser(DatalakeBRKChecker, EMUBlackrockDJUploader, ParserCommon):
     """Parser for inserting new BRK data that arrives in the data lake into DataJoint"""
