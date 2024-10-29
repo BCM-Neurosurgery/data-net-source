ec98898ddc42625e7fbd9fb0b571c16e12bd56ce
Hegelim <thefirstzyw@hotmail.com>
2024-10-29 10:17:22 -0500

    Remove parsers/trbd.py

--- parsers/trbd.py
+++
@@ -1,7 +0,0 @@
-from source.checkers.local import FileCheckerMixin
-
-
-class TRBDChecker(FileCheckerMixin):
-    streamed_files = [".json"]
-    stream_rate = 24 * 60 * 60  # New file every 24 hours
-    reliability_factor = 1.1  # File durations are quite reliable, approx 1 min buffer in case of rounding error
