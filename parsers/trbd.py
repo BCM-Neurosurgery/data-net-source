8f1f07689a26ffe5f5386faff206c0f6a9c9cfec
Hegelim <thefirstzyw@hotmail.com>
2024-10-29 09:57:46 -0500

    Remove TRBDRemoteParser and TRBDLocalParser

--- parsers/trbd.py
+++ parsers/trbd.py
@@ -1,23 +1,7 @@
-from source.common import ParserCommon
-from source.checkers.local import StreamedFileCheckerMixin
-from source.uploaders.simple import CopyUploaderMixin
-from source.uploaders.ssh import SCPUploaderMixin
+from source.checkers.local import FileCheckerMixin
 
 
-class TRBDChecker(StreamedFileCheckerMixin):
-    streamed_files = [
-        "daily_activity.json",
-        "daily_sleep.json",
-        "daily_stress.json",
-        "sleep.json",
-    ]
+class TRBDChecker(FileCheckerMixin):
+    streamed_files = [".json"]
     stream_rate = 24 * 60 * 60  # New file every 24 hours
     reliability_factor = 1.1  # File durations are quite reliable, approx 1 min buffer in case of rounding error
-
-
-class TRBDRemoteParser(TRBDChecker, SCPUploaderMixin, ParserCommon):
-    """Parser for uploading new TRBD data to the remote server"""
-
-
-class TRBDLocalParser(TRBDChecker, CopyUploaderMixin, ParserCommon):
-    """Parser for copying new TRBD data to the local backup"""
