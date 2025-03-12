4ee814b74351e54d6650f4c4097925f87d14611d
TomekFraczek <tfraczek@uw.edu>
2025-03-12 15:37:22 -0500

    Fix ABC behavior of new indicators

--- source/checkers/local/indicator.py
+++ source/checkers/local/indicator.py
@@ -1,12 +1,20 @@
+import abc
 import os
 import re
 import toml
+from abc import ABC, abstractmethod
 
 from source.checkers.local.base import FileCheckerMixin
 
 
-class BaseIndicatorChecker(FileCheckerMixin):
+class BaseIndicatorChecker(FileCheckerMixin, ABC):
     """"""
+
+    @abstractmethod
+    def parse_indicators(self):
+        """Implement this to determine which sub-directories in the source directory to include in the check"""
+        pass
+
 
     def check(self):
         """Recursively check the contents of a subset of the directories in the given path"""
@@ -17,7 +25,7 @@ class BaseIndicatorChecker(FileCheckerMixin):
 
         for directory in to_check:
             try:
-                found_here = super(IndicatorFileCheckerMixin, self).search_file_tree(source_dir=directory, level=1)
+                found_here = super(BaseIndicatorChecker, self).search_file_tree(source_dir=directory, level=1)
             except FileNotFoundError as e:
                 raise FileNotFoundError(f'Indicator file suggested an invalid path: \n  {e.filename}')
             to_do.extend(found_here['to do'])
