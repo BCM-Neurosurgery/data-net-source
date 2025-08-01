ac745fd411e68905c7a7f14ef3a040b7f75f9a01
TomekFraczek <tfraczek@uw.edu>
2025-08-01 17:48:34 +0200

    Added in config stub

--- source/uploaders/json.py
+++ source/uploaders/json.py
@@ -5,6 +5,8 @@ import json
 from pathlib import Path
 from datetime import datetime
 
+import toml
+
 from source.uploaders.base import BaseUploader
 
 class JSONDocInjectorUploader(BaseUploader):
@@ -22,14 +24,19 @@ class JSONDocInjectorUploader(BaseUploader):
 
     """
 
-    #: Path key to the unique identifier of this payload
-    doc_id_key = ""
+    config_stub = """
+    [parser.init.target]
+    path = '/base/path/for/all/jsons' 
+    # Key in the payload that contains the unique identifier of this payload
+    doc_id_key = 'document_id'
+    # relative path/filename of the JSON to inject data into (need not exist) as a python format string
+    # Filled with context obtained from both the payload and the path metadata
+    new_format = {patient_id}/oura/{date}/{doc_type}.json
+    """
+    target_location = toml.loads(config_stub)['parser']['init']['target']  # Load a default config for type hints
 
     #: Regex describing how to extract metadata from the source file path. Must include named groups
-    source_regex = ""
-
-    #:
-    new_format = ""
+    source_regex = None
 
     def load_source(self, source_path):
         """
