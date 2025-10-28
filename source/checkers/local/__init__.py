362c828cd6342d3ccfbd01b8fc18561185f83294
NikhilDS-Rice <nd66@rice.edu>
2025-10-28 16:51:29 -0500

    Re-established the existing parser behaviour and made sure there's enough logic to handle if  check_for_modifications is not specified

--- source/checkers/local/__init__.py
+++ source/checkers/local/__init__.py
@@ -112,12 +112,8 @@ class FileCheckerMixin(BaseChecker):
         # Draw the source location from the class settings if not passed explicitly under recursion
         source_dir = self.source_location['path'] if source_dir is None else source_dir
 
-        # check_for_modifications is required in the config file
-        if 'check_for_modifications' in self.source_location:
-            check_modified = self.source_location['check_for_modifications']
-        else:
-            check_modified = False
-
+        # Defaults to False if not present, maintaining the original behavior.
+        check_modified = self.source_location.get('check_for_modifications', False)
 
         successes = self.load_non_failure()
         
