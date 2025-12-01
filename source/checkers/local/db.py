6a4a2ed92ef84337df186bdb2730134c269bfd0d
Taha Ismail <taha.ismail424@gmail.com>
2025-12-01 17:34:43 -0600

    changed datetime objects to be pandas timestamps

--- source/checkers/local/db.py
+++ source/checkers/local/db.py
@@ -1,22 +1,21 @@
 import os
 from datetime import datetime
 import requests
-from datetime import datetime, timedelta, time, date
+import pandas as pd
 from source.checkers.local import FileCheckerMixin
 
 class DBFileCheckerMixin(FileCheckerMixin):
     db_url = ''
     dump_route = ''
-    # look back duration in minutes, default to 24 hours
-    look_back_duration = 1440
+    # look back duration as pandas timedelta str
+    look_back_duration = '1D'
 
     # by default gets data from midnight to 'look_back_duration' prior
     # method can be overwritten for custom payload behavior
     def get_payload(self):
-        current_date = date.today()
-        midnight_local = datetime.combine(current_date, time.min)
+        midnight_local = pd.Timestamp("today").normalize()
         # now get look_back_duration prior
-        time_prior = midnight - timedelta(minutes=look_back_duration)
+        time_prior = midnight - pd.Timedelta(look_back_duration)
         payload = {
             "query_start": time_prior.isoformat(),
             "query_end": midnight_local.isoformat()
